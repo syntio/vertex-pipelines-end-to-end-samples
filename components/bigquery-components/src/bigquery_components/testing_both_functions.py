a539a8dbe974877bba06b07f2ceb1c05810d7816
Mihovil Mađerić <mihovil.maderic@syntio.net>
2025-07-28 10:04:56 +0200

    Dodane dvije test skripte

---
+++ components/bigquery-components/src/bigquery_components/testing_both_functions.py
@@ -0,0 +1,52 @@
+from kfp import dsl, compiler
+from google.cloud import aiplatform
+from .bq_query_to_table import bq_query_to_table
+from .extract_bq_to_dataset import extract_bq_to_dataset
+
+@dsl.pipeline(
+    name="extract-and-export-chicago-taxi-data",
+    description="Query Chicago taxi data and export result to GCS",
+)
+def extract_pipeline():
+    query_task = bq_query_to_table(
+        query="SELECT * FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips` WHERE trip_total = 13 LIMIT 10",
+        bq_client_project_id="syntio-ai-ops",
+        destination_project_id="syntio-ai-ops",
+        dataset_id="chicago_taxi_trips",
+        table_id="taxi_trips_sample_2",
+        dataset_location="US",
+        query_job_config={
+            "write_disposition": "WRITE_TRUNCATE"
+        }
+    )
+
+    extract_task = extract_bq_to_dataset(
+        bq_client_project_id="syntio-ai-ops",
+        source_project_id="syntio-ai-ops",
+        dataset_id="chicago_taxi_trips",
+        table_name="taxi_trips_sample_2",
+        dataset_location="US",
+        destination_gcs_uri="gs://test_bucket_for_bigquery_123/exported_data/taxi_trips_sample_2.csv",
+
+    )
+    extract_task.after(query_task)
+
+if __name__ == "__main__":
+    from datetime import datetime
+
+    pipeline_filename = "extract_export_pipeline.json"
+    job_id = f"extract-chicago-taxi-data-job-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
+
+    compiler.Compiler().compile(
+        pipeline_func=extract_pipeline,
+        package_path=pipeline_filename,
+    )
+
+    aiplatform.init(project="syntio-ai-ops", location="us-central1")
+
+    aiplatform.PipelineJob(
+        display_name="extract-and-export-chicago-taxi-data",
+        template_path=pipeline_filename,
+        pipeline_root="gs://test_bucket_for_bigquery_123/pipeline-root",
+        job_id=job_id,
+    ).run()
