a6e255d4ca17a97c0470bc66ffd5a55c63868a04
mihovil-maderic <mihovil.maderic@syntio.net>
2025-08-01 15:47:10 +0200

    Update testing_with_dq_check.py

---
+++ components/bigquery-components/src/bigquery_components/testing_with_dq_check.py
@@ -0,0 +1,67 @@
+from kfp import dsl, compiler
+from google.cloud import aiplatform
+from .bq_query_to_table import bq_query_to_table
+from .extract_bq_to_dataset import extract_bq_to_dataset
+from .run_auto_dq_scan import run_auto_dq_scan
+
+def sanitize(s: str) -> str:
+    return s.encode("utf-8", "strict").decode("utf-8")
+
+
+@dsl.pipeline(
+    name="extract-and-export-and-check-chicago-taxi-data",
+    description="Query Chicago taxi data, check it and export result to GCS",
+)
+def extract_pipeline():
+    query_task = bq_query_to_table(
+        query="SELECT * FROM `syntio-ai-ops.chicago_taxi_trips.taxi_trips` WHERE trip_total = 13 LIMIT 10",
+        bq_client_project_id="syntio-ai-ops",
+        destination_project_id="syntio-ai-ops",
+        dataset_id="chicago_taxi_trips",
+        table_id="taxi_trips_sample_2",
+        dataset_location="europe-west1",
+        query_job_config={
+            "write_disposition": "WRITE_TRUNCATE"
+        }
+    )
+
+    extract_task = extract_bq_to_dataset(
+        bq_client_project_id="syntio-ai-ops",
+        source_project_id="syntio-ai-ops",
+        dataset_id="chicago_taxi_trips",
+        table_name="taxi_trips_sample_2",
+        dataset_location="europe-west1",
+        destination_gcs_uri="gs://test-for-bigquery-eu-123/exported_data/taxi_trips_sample_2.csv",
+
+    )
+
+    dq_scan = run_auto_dq_scan(
+        #project_id=sanitize("syntio-ai-ops"),
+        #location=sanitize("europe-west1"),
+        #bq_table=sanitize("syntio-ai-ops.chicago_taxi_trips.taxi_trips_sample_2"),
+        #dq_scan_id=sanitize("taxi-trips-auto-dq-scan"),
+        #profile_scan_id=sanitize("taxi-trips-profile-scan"),
+    )
+
+    dq_scan.after(query_task)
+    extract_task.after(dq_scan)
+
+if __name__ == "__main__":
+    from datetime import datetime
+
+    pipeline_filename = "extract_export_check_pipeline.json"
+    job_id = f"extract-chicago-taxi-data-job-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
+
+    compiler.Compiler().compile(
+        pipeline_func=extract_pipeline,
+        package_path=pipeline_filename,
+    )
+
+    aiplatform.init(project="syntio-ai-ops", location="europe-west1")
+
+    aiplatform.PipelineJob(
+        display_name="extract-and-export-and-check-chicago-taxi-data",
+        template_path=pipeline_filename,
+        pipeline_root="gs://test-for-bigquery-eu-123/pipeline-root",
+        job_id=job_id,
+    ).run()
