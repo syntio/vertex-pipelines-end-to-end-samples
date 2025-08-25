32d03de3c5be64ca8fdd8f3ac05265b8dbcb35aa
matea.vlahovic <matea.vlahovic@datatonic.com>
2025-08-25 05:43:51 -0700

    Resolve init issue

--- components/bigquery-components/src/bigquery_components/__init__.py
+++ components/bigquery-components/src/bigquery_components/__init__.py
@@ -1,5 +1,5 @@
-from bq_query_to_table import bq_query_to_table
-from extract_bq_to_dataset import extract_bq_to_dataset
+from .bq_query_to_table import bq_query_to_table
+from .extract_bq_to_dataset import extract_bq_to_dataset
 
 
 __version__ = "0.0.1"
