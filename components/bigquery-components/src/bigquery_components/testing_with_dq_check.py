fca910fc160680982a7c8fee787d0a80c447f6ea
IvanVSyntio <ivan.zeljeznjak@syntio.net>
2025-09-03 13:52:18 +0200

    Remove hardcoded project refs from bigquery-components

--- components/bigquery-components/src/bigquery_components/testing_with_dq_check.py
+++ components/bigquery-components/src/bigquery_components/testing_with_dq_check.py
@@ -1,3 +1,4 @@
+import os
 from kfp import dsl, compiler
 from google.cloud import aiplatform
 from bq_query_to_table import bq_query_to_table
@@ -14,11 +15,11 @@ from datetime import datetime
 def extract_pipeline():
     query_task = bq_query_to_table(
         query=(
-            "SELECT * FROM `syntio-ai-ops.chicago_taxi_trips.taxi_trips`"
+            f"SELECT * FROM `{os.environ.get('PROJECT_ID')}.chicago_taxi_trips.taxi_trips`"
             "WHERE trip_total = 13 LIMIT 10"
         ),
-        bq_client_project_id="syntio-ai-ops",
-        destination_project_id="syntio-ai-ops",
+        bq_client_project_id=os.environ.get("PROJECT_ID"),
+        destination_project_id=os.environ.get("PROJECT_ID"),
         dataset_id="chicago_taxi_trips",
         table_id="taxi_trips_sample_2",
         dataset_location="europe-west1",
@@ -26,8 +27,8 @@ def extract_pipeline():
     )
 
     extract_task = extract_bq_to_dataset(
-        bq_client_project_id="syntio-ai-ops",
-        source_project_id="syntio-ai-ops",
+        bq_client_project_id=os.environ.get("PROJECT_ID"),
+        source_project_id=os.environ.get("PROJECT_ID"),
         dataset_id="chicago_taxi_trips",
         table_name="taxi_trips_sample_2",
         dataset_location="europe-west1",
@@ -37,9 +38,9 @@ def extract_pipeline():
     )
 
     dq_scan = run_scan(
-        project_id="syntio-ai-ops",
+        project_id=os.environ.get("PROJECT_ID"),
         location="europe-west1",
-        bq_table="syntio-ai-ops.chicago_taxi_trips.taxi_trips_sample_2",
+        bq_table=f"{os.environ.get('PROJECT_ID')}.chicago_taxi_trips.taxi_trips_sample_2",
         dq_scan_id=f"taxi-trips-scan-{datetime.now().strftime('%Y%m%d-%H%M%S')}",
     )
 
@@ -57,7 +58,7 @@ if __name__ == "__main__":
         package_path=pipeline_filename,
     )
 
-    aiplatform.init(project="syntio-ai-ops", location="europe-west1")
+    aiplatform.init(project=os.environ.get("PROJECT_ID"), location="europe-west1")
 
     aiplatform.PipelineJob(
         display_name="extract-and-export-and-check-chicago-taxi-data",
